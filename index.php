ee71988092ea17046ef26dbc658fc5e4ffa67bc2
theboxmaker <ztucker99@gmail.com>
2025-11-23 18:08:36 -0500

    update index.php

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
 <?php
-echo "<h1>Hello from Zach's PHP site</h1>";
-echo "<p>If you can see this, Render is running PHP correctly.</p>";
+// Use absolute paths to ensure includes always work
+$base = __DIR__;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="web250/styles/default.css">
+    <title>Zachary Tucker's Web Projects - Home</title>
+    <script src="https://lint.page/kit/67ff88.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <?php include "$base/web250/components/header.html"; ?>
+
+    <main>
+        <h2>Home</h2>
+        <p>
+            Hello! I am Zachary Tucker, a 26 year old student learning web development.
+            My goal is to complete my schooling and enter the workforce.
+            I like to spend my free time outside, either rock climbing or golfing!
+            This site will be home to any and all projects I work on for the foreseeable future!
+        </p>
+
+        <p><a href="web250/">Click to go to my WEB250 class</a></p>
+
+        <p>
+            You can learn more about me by visiting my
+            <a href="web250/introduction.php">introduction</a>.
+        </p>
+    </main>
+
+    <?php include "$base/web250/components/footer.html"; ?>
+
+</body>
+</html>
