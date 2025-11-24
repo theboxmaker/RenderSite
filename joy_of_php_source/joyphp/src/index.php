55e36075bdd996d9c0242c89fad9a35883250889
theboxmaker <ztucker99@gmail.com>
2025-11-23 22:29:04 -0500

    joyofphp index and CreateDB

--- joy_of_php_source/joyphp/src/index.php
+++ joy_of_php_source/joyphp/src/index.php
@@ -1,68 +1,122 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta content="en-us" http-equiv="Content-Language" />
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Welcome to the Joy of PHP</title>
-<style type="text/css">
-.auto-style1 {
-	border-style: solid;
-	border-width: 1px;
-	background-color: #C0C0C0;
-}
-</style>
+    <meta charset="UTF-8">
+    <title>Welcome to the Joy of PHP</title>
+
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            background: url('bg.jpg') no-repeat center top fixed;
+            background-size: cover;
+            margin: 0;
+            padding: 20px;
+            line-height: 1.6;
+            color: #222;
+        }
+
+        h2 {
+            color: #2f4f4f;
+            margin-top: 30px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin: 15px 0;
+        }
+
+        .auto-style1 {
+            border: 1px solid #888;
+            background-color: #c0c0c0;
+        }
+
+        td, th {
+            padding: 12px;
+            vertical-align: top;
+        }
+
+        a {
+            color: #004c66;
+            font-weight: bold;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #007799;
+            text-decoration: underline;
+        }
+
+        img.header-logo {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin-bottom: 15px;
+        }
+
+        img.book-cover {
+            display: block;
+            margin-top: 25px;
+            max-width: 200px;
+            height: auto;
+        }
+    </style>
 </head>
 
-<body background="bg.jpg">
+<body>
 
-<p><img alt="The Joy of PHP" height="87" src="joy_logo.jpg" width="809" /></p>
-<h2>Introduction</h2>
-<p>The Joy of PHP book walks you through creating a website for <strong>
-<a href="samsusedcars.html">Sam's Used Cars</a></strong>.&nbsp; 
-All the sample code in the book is conveniently created here.&nbsp; To get 
-started, you'll need to make sure that you have PHP and mySQL running on your 
-machine.&nbsp; Here are the links to get started.&nbsp; </p>
-<h2>Getting Started</h2>
-<table style="width: 100%" class="auto-style1">
-	<tr>
-		<td style="width: 377px"><strong>Prerequisites</strong></td>
-		<td><strong>Free Code Editors</strong></td>
-	</tr>
-	<tr>
-		<td style="width: 377px">
-		<p>PC Users: <a href="http://www.wampserver.com/en/">Download WAMP</a></p>
-		<p>Mac Users:
-		<a href="http://www.apachefriends.org/en/xampp-macosx.html">Download 
-		XAMP</a></p>
-		<p>&nbsp;</p>
-		</td>
-		<td>
-		<p STYLE="margin-bottom: 0in"><a HREF="http://www.pnotepad.org/">
-		Programmer's Notepad</a> is a good all purpose editor</p>
-		<p STYLE="margin-bottom: 0in"><a HREF="http://devphp.sourceforge.net/">
-		Dev-PHP</a> is a PHP-specific editor</p>
-		<p STYLE="margin-bottom: 0in">
-		<a HREF="http://www.barebones.com/products/textwrangler/">TextWrangler</a> 
-		is a good editor for the Mac.</p>
-		</td>
-	</tr>
-</table>
-<h2>Following along with the lessons</h2>
-<p>Is PHP Running on my machine?  What version?  What extensions? &nbsp; Check it here -->
-<a href="phpinfo.php" target="_blank">PHP Info Paget</a></p>
-<p>A really simple PHP page --&gt; <a href="simple.php">simple.php</a></p>
-<h2>Database Related Scripts</h2>
-<p><a href="CreateDB.php">Create the Cars Database</a> (Before you can use this, 
-you'll need to edit line 8 with your username and password)</p>
+    <img src="joy_logo.jpg" alt="The Joy of PHP" class="header-logo">
 
-<p><a href="ModifyDB.php">Modify the Inventory Table</a> (This script adds the
-<strong>Primary_Image</strong> column.&nbsp; Again, edit line 8 with your 
-username and password)</p>
-<p><a href="CreateImagesTable.php">Create the Images Table</a> (Again, edit line 
-8 with your username and password)</p>
-<p><img alt="Joy of PHP" height="313" src="smallcover.jpg" width="196" /></p>
+    <h2>Introduction</h2>
+    <p>
+        The <strong>Joy of PHP</strong> book walks you through creating a website for 
+        <strong><a href="samsusedcars.html">Sam's Used Cars</a></strong>.
+        All the sample code in the book is conveniently collected here.
+        To get started, make sure you have PHP and MySQL running on your machine.
+    </p>
+
+    <h2>Getting Started</h2>
+
+    <table class="auto-style1">
+        <tr>
+            <th style="width: 40%;">Prerequisites</th>
+            <th>Free Code Editors</th>
+        </tr>
+        <tr>
+            <td>
+                <p>PC Users: <a href="http://www.wampserver.com/en/">Download WAMP</a></p>
+                <p>Mac Users: <a href="http://www.apachefriends.org/en/xampp-macosx.html">Download XAMPP</a></p>
+            </td>
+
+            <td>
+                <p><a href="http://www.pnotepad.org/">Programmer's Notepad</a> — good all-purpose editor</p>
+                <p><a href="http://devphp.sourceforge.net/">Dev-PHP</a> — PHP-specific editor</p>
+                <p><a href="http://www.barebones.com/products/textwrangler/">TextWrangler</a> — great for Mac OS</p>
+            </td>
+        </tr>
+    </table>
+
+    <h2>Following Along with the Lessons</h2>
+    <p>
+        Check your PHP version and configuration:
+        <a href="phpinfo.php" target="_blank">PHP Info Page</a>
+    </p>
+
+    <p>
+        Try a simple PHP page:
+        <a href="simple.php">simple.php</a>
+    </p>
+
+    <h2>Database-Related Scripts</h2>
+    <p><a href="CreateDB.php">Create the Cars Database</a><br>
+       (Edit line 8 with your username & password first.)</p>
+
+    <p><a href="ModifyDB.php">Modify the Inventory Table</a><br>
+       (Adds the <strong>Primary_Image</strong> column.)</p>
+
+    <p><a href="CreateImagesTable.php">Create the Images Table</a></p>
+
+    <img src="smallcover.jpg" alt="Joy of PHP Book Cover" class="book-cover">
 
 </body>
-
 </html>
