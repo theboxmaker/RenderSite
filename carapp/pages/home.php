ecaef2e62f68399132d22b2cf416f7f0bfde1110
theboxmaker <ztucker99@gmail.com>
2025-12-04 16:50:28 -0500

    home php udpate

--- carapp/pages/home.php
+++ carapp/pages/home.php
@@ -1,12 +1,2 @@
-<?php
-$title = "Welcome";
-ob_start();
-?>
-
 <h2>Welcome to Zany Terrapin’s Car Lot</h2>
 <p>Browse the inventory or add a new vehicle.</p>
-
-<?php
-$content = ob_get_clean();
-include __DIR__ . "/../components/header.php";
-
