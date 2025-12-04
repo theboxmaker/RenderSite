b5e333fb22351abfacc28111c7ddaea1f00fc3ba
theboxmaker <ztucker99@gmail.com>
2025-12-04 12:48:00 -0500

    update config_db

--- carapp/config_db.php
+++ carapp/config_db.php
@@ -1,10 +1,37 @@
 <?php
-$DB_HOST = "127.0.0.1";  // Required for XAMPP
-$DB_USER = "root";       // XAMPP default user
-$DB_PASS = "";           // XAMPP default has NO password
-$DB_NAME = "railway";    // <-- Change if your database has a different name
+$isRenderOrDocker = true; // containerized environment ALWAYS remote DB
 
-$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+$DB_HOST = $_ENV['DB_HOST'];
+$DB_PORT = $_ENV['DB_PORT'];
+$DB_USER = $_ENV['DB_USER'];
+$DB_PASS = $_ENV['DB_PASS'];
+$DB_NAME = $_ENV['DB_NAME'];
+$DB_SSL  = $_ENV['DB_SSL'] ?? null;
+
+$mysqli = mysqli_init();
+
+if ($DB_SSL) {
+    mysqli_ssl_set($mysqli, null, null, $DB_SSL, null, null);
+    mysqli_real_connect(
+        $mysqli,
+        $DB_HOST,
+        $DB_USER,
+        $DB_PASS,
+        $DB_NAME,
+        (int)$DB_PORT,
+        null,
+        MYSQLI_CLIENT_SSL
+    );
+} else {
+    mysqli_real_connect(
+        $mysqli,
+        $DB_HOST,
+        $DB_USER,
+        $DB_PASS,
+        $DB_NAME,
+        (int)$DB_PORT
+    );
+}
 
 if ($mysqli->connect_errno) {
     die("Database connection failed: " . $mysqli->connect_error);
