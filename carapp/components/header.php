9fc19a0e6d339fc7d649d56a5182c7065c04bc16
theboxmaker <ztucker99@gmail.com>
2025-12-04 17:01:51 -0500

    update header links

--- carapp/components/header.php
+++ carapp/components/header.php
@@ -2,7 +2,7 @@
     <h1>Zany Terrapin's Used Cars</h1>
     <nav>
         <a href="<?= BASE_URL ?>/?page=home">Home</a>
-        <a href="<?= BASE_URL ?>/?page=cars_list">Inventory</a>
-        <a href="<?= BASE_URL ?>/?page=car_add">Add Car</a>
+        <a href="<?= BASE_URL ?>/?page=carsList">Inventory</a>
+        <a href="<?= BASE_URL ?>/?page=carAdd">Add Car</a>
     </nav>
 </header>
