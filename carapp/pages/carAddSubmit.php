43827f2fb10bc11abe7471bdf6e0944ed084a166
theboxmaker <ztucker99@gmail.com>
2025-12-05 13:39:05 -0500

    update carAddSubmit.php

--- carapp/pages/carAddSubmit.php
+++ carapp/pages/carAddSubmit.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../config_db.php';
 require_once APP_PATH . '/db.php';
-require_once APP_PATH . '/models/CarModel.php';
+require_once APP_PATH . '/models/carModel.php';
 
 CarModel::add($pdo, $_POST);
 
